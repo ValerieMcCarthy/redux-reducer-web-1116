41b01848f1f231353a5f2d1cff50b9d50f0d05b7
Valerie McCarthy <valeriewilsonmccarthy@gmail.com>
2017-02-06 20:54:20 -0500

    done

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+  if (action.type === "ADD_FRIEND") {
+    return Object.assign({}, state, state.friends.push(action.payload))
+  } else if (action.type === "REMOVE_FRIEND") {
+    console.log(action.payload);
+    let newFriends = state.friends.filter((friend) => {
+      return friend.id !== action.payload
+    })
+    return Object.assign({}, state, {friends: newFriends})
+  } else {
+    return state
+  }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,11 @@
 export function managePresents(state, action){
+		if (action.type === 'INCREASE'){
+		return (Object.assign({}, {numberOfPresents: state.numberOfPresents+1}))
+	} else {
+		return state
+	}
+
 }
+
+
+
